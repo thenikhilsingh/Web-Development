bb1c69bf26f84dbb3c2aec557782103e78be8aab
Nikhil Singh <nikhilsingh26042004@gmail.com>
2024-12-18 11:12:20 +0530

    modified index.html and script.js of Exercise 12 - Color the Boxes

--- JavaScript/Exercise 12 - Color the Boxes/script.js
+++ JavaScript/Exercise 12 - Color the Boxes/script.js
@@ -1,1 +1,13 @@
 /* Given 5 boxes, Assign a random color and a random background color to each box using DOM concepts */
+let boxes = document.querySelector(".container").children;
+function getRandomColor() {
+  let val1 = Math.ceil(0 + Math.random() * 255);//Math.ceil() is used to round off numbers
+  let val2 = Math.ceil(0 + Math.random() * 255);//Math.random() is used to generate random numbers
+  let val3 = Math.ceil(0 + Math.random() * 255);
+  return `rgb(${val1},${val2},${val3})`;
+}
+//converted boxes into array because we can't apply for loop on HTML collections
+Array.from(boxes).forEach((e) => {
+  e.style.backgroundColor = getRandomColor();
+  e.style.color = getRandomColor();
+});
