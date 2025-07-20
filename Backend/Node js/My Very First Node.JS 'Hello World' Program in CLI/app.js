68b7c398505ca91013917a630113e649c5a7cc99
thenikhilsingh <149455116+thenikhilsingh@users.noreply.github.com>
2025-07-20 11:59:14 +0530

    added My Very First Node.JS 'Hello World' Program in CLI folder

---
+++ Backend/Node js/My Very First Node.JS 'Hello World' Program in CLI/app.js
@@ -0,0 +1,6 @@
+console.log("Hello World");
+//To run this code using node:
+// node file name
+//for this code we type "node app.js" to run this code
+const arr = [1, 2, 3, 4, 5];
+console.log(arr.map((num) => num * 5));
