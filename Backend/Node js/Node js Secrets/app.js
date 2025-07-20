d74d4f32cc52f05f9c174074690a93f8cae9f929
thenikhilsingh <149455116+thenikhilsingh@users.noreply.github.com>
2025-07-20 18:01:17 +0530

    added Node js Secrets Folder

---
+++ Backend/Node js/Node js Secrets/app.js
@@ -0,0 +1,11 @@
+// console.log(global);
+global.console.log("subscribe");
+globalThis.console.log("subscribe");
+console.log(globalThis); //globalThis alg alg environments ke alg alg global objects ka roop dharan kar leta h
+globalThis.console.log(module);
+// console.log(globalThis.module); //nhi chalega undefined ayega kyunki module apne aap me separate h globalThis ka part nhi h ye
+//module hum tbhi use kar sakte hai jb common js use karte h
+console.log(globalThis.process);
+
+//React - Components
+//Node - Modules
