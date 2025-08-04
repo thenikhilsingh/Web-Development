c093519117a2ebf1b6d3d889edb36031a85e9e39
thenikhilsingh <149455116+thenikhilsingh@users.noreply.github.com>
2025-08-04 13:49:39 +0530

    adding ESModules in Node.js Default, Named, and Aliased Imports Explained folder

---
+++ Backend/Node js/ESModules in Node.js Default, Named, and Aliased Imports Explained/app.js
@@ -0,0 +1,15 @@
+// import mul from "./math.js";
+// import { mul, div } from "./math.js";
+// import { add, sub, mul, div, PI } from "./math.js";
+import * as math from "./math.js";
+
+// console.log(add(5, 10));
+// console.log(sub(5, 10));
+// console.log(mul(5, 10)); //you export mul as default so you can give any name to it. It will perform multiplication.for ex:- console.log(multiply(60,10))
+// console.log(div(5, 10));
+// console.log(PI);
+console.log(math.add(5, 10));
+console.log(math.sub(5, 10));
+console.log(math.mul(5, 10)); 
+console.log(math.div(5, 10));
+console.log(math.PI);
---
+++ Backend/Node js/ESModules in Node.js Default, Named, and Aliased Imports Explained/math.js
@@ -0,0 +1,26 @@
+const add = (a, b) => {
+  return a + b;
+};
+
+const sub = (a, b) => {
+  return a - b;
+};
+
+// export const mul = (a, b) => {
+//   return a * b;
+// };
+ const mul = (a, b) => {
+  return a * b;
+};
+
+// export const div = (a, b) => {
+//   return a / b;
+// };
+ const div = (a, b) => {
+  return a / b;
+};
+
+const PI = 3.124;
+
+// export default mul;
+export { add, sub, mul, div, PI };
