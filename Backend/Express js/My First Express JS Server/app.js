3ae100d00492737a96025f47efe006298c2a1879
thenikhilsingh <149455116+thenikhilsingh@users.noreply.github.com>
2025-08-16 19:08:13 +0530

    adding My First Express JS Server folder

---
+++ Backend/Express js/My First Express JS Server/app.js
@@ -0,0 +1,31 @@
+import express from "express";
+
+const app = express();
+// app → This variable holds the created Express app,
+// which you can use to:
+
+// Define routes (app.get(), app.post(), etc.)
+// Configure middleware (app.use())
+// Start the server (app.listen())
+
+app.get("/", (req, res) => res.send("<h1>Hello World!</h1>"));
+app.get("/about", (req, res) => res.send("<h1>Hello About Page!</h1>"));
+app.get("/contact", (req, res) => {
+  return res.send(`
+    <h1>Contact Us</h1>
+    <form action="/contact" method="post">
+      <label for="name">Name:</label><br>
+      <input type="text" id="name" name="name" required><br><br>
+      <label for="email">Email:</label><br>
+      <input type="email" id="email" name="email" required><br><br>
+      <label for="message">Message:</label><br>
+      <textarea id="message" name="message" rows="4" required></textarea><br><br>
+      <button type="submit">Send</button>
+    </form>
+  `);
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`server is running at port: ${PORT}`);
+});
