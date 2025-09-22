000dbc1168cba84055dadfcac160cab140511a12
thenikhilsingh <149455116+thenikhilsingh@users.noreply.github.com>
2025-09-22 14:46:16 +0530

    adding Introduction to Express folder

---
+++ Backend/Expressjs The Odin Project/Introduction to Express/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("Hello, World!"));
+
+const PORT = 3000;
+app.listen(PORT, (error) => {
+  // This is important!
+  // Without this, any startup errors will silently fail
+  // instead of giving you a helpful error message.
+  if (error) {
+    throw error;
+  }
+  console.log(`My first Express app - listening on port ${PORT}!`);
+});
