958c82fe400b7635d23b20c59f57337aa1b693ee
thenikhilsingh <nikhilsingh26042004@gmail.com>
2025-12-02 15:15:57 +0530

    adding how-to-hide-navbar-on-specific-pages folder

---
+++ Frontend/Next js/how-to-hide-navbar-on-specific-pages/app/about/page.jsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function About() {
+  return (
+    <>
+      <h1>About Page</h1>
+      <button><Link href="/">Home Page</Link></button>
+    </>
+  );
+}
